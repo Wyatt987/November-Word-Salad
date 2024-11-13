a1ee63cb3c1a26074c2cbc8ca3bf9305d9f4568f
wyatt987 <526wbedor@frhsd.com>
2024-11-13 09:30:08 -0500

    Started Story

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let userName = prompt('What is your name?')
-let userAge = prompt('How old are you?')
+let bestfriend = prompt('Name')
+let wayOfTravelAcrossSea = prompt('A Way to Travel Across Sea?')
+let numberOfHours = prompt('Number of Hours?')
+let placeToStay = prompt('A Place to Stay?')
+let somethingToDoAtTheBeachInPastTense = prompt('Something To Do at the Beach (in past tense)?')
+
+
 console.log(userName)
 console.log(userAge)
 
-let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge} years old?`
+let myStory = `Me and my bestfriend ${bestfriend} went on a trip to the Bahammas. The ${wayOfTravelAcrossSea} ride was so long! In total I think it took ${numberOfHours} HOURS! Once we finally got there, we checked into our ${placeToStay}. I went straight to the beach and ${somethingToDoAtTheBeachInPastTense} for the rest of the day.`
 console.log(myStory)
 
 document.getElementById('story').innerHTML = myStory;
