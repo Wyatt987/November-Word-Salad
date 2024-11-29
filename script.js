a6b9374b220e6b0e9a13247d286ad7678f9394cc
Wyatt987 <526wbedor@frhsd.com>
2024-11-29 18:32:53 +0000

    Seperated stories

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ let Thirteenth = prompt('different object');
 let Fourteenth = prompt('something to lay on');
 let Fifteenth = prompt('adjective');
 
-let myStory = `Last week, it was just like any other day, when BAM! I broke my ${First}! I got rushed to ${Second} to get treated asap. Before they did anything, the numbed my body using a ${Third} and injected me with ${Fourth}. Once it was injected into me, everything started spinning, my ${Fifth} started aching extremely bad. ${Sixth}, I passed out. When I woke up, I was in a strange room filled with ${Seventh}s. A nurse handed me a ${Eighth} and told me to throw it at the ${Ninth}s! I was so confused! Just as I was about to refuse, my ${Tenth} did it on its own! A doctor bursts in and shouted, "Don't let the ${Eleventh}s escape!" Suddenly, everything turned into ${Twelth}s and ${Thirteenth}, and I realized it was all a dream. I opened my eyes, and realized I was laying in ${Fourteenth}, and that the surgery went ${Fifteenth}.`;
+let myStoryPart1 = `Last week, it was just like any other day, when BAM! I broke my ${First}! I got rushed to ${Second} to get treated asap. Before they did anything, the numbed my body using a ${Third} and injected me with ${Fourth}. Once it was injected into me, everything started spinning, my ${Fifth} started aching extremely bad. ${Sixth}, I passed out.`;
 
-document.getElementById('story').innerHTML = myStory;
+document.getElementById('story').innerHTML = myStoryPart1;
+
+let myStoryPart2 = `When I woke up, I was in a strange room filled with ${Seventh}s. A nurse handed me a ${Eighth} and told me to throw it at the ${Ninth}s! I was so confused! Just as I was about to refuse, my ${Tenth} did it on its own! A doctor bursts in and shouted, "Don't let the ${Eleventh}s escape!" Suddenly, everything turned into ${Twelth}s and ${Thirteenth}, and I realized it was all a dream. I opened my eyes, and realized I was laying in ${Fourteenth}, and that the surgery went ${Fifteenth}.`
+
+document.getElementById('story').innerHTML = myStoryPart2;
 
 //scroll
 scrollBtn.addEventListener('click', () => {
